f8512956e1a2398cd00b1662b40e40c41d847b95
Abhineet <abhineet.am13@gmail.com>
2025-10-11 17:10:18 +0530

    Add javaScript file

---
+++ JavaScript/basic.js
@@ -0,0 +1,33 @@
+var x=4
+var y=5
+var z=x+y
+console.log(z)
+
+var a="Hello"
+var b=" World"
+var c=a+b
+console.log(c)
+
+var your_name =prompt("Enter your name")
+console.log("Hello "+your_name)
+
+var age = prompt("Enter your age")
+age = Number.parseInt(age)
+console.log("You are "+age+" years old")
+
+if(age>18)
+    console.log("ADULT")
+else if(age==18)
+    console.log("JUST  ADULT")
+else
+    console.log("NOT ADULT")
+
+var i
+for(i=0;i<5;i++)
+    console.log(i)
+var j=0
+while(j<5)
+{
+    console.log(j)
+    j++
+}
