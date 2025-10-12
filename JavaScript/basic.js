2131f5d7dac2994929ab1f35abdb03225cfcff47
itsam-13 <abhineet.am13@gmail.com>
2025-10-12 21:35:29 +0530

    update

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -15,13 +15,6 @@ var age = prompt("Enter your age")
 age = Number.parseInt(age)
 console.log("You are "+age+" years old")
 
-if(age>18)
-    console.log("ADULT")
-else if(age==18)
-    console.log("JUST  ADULT")
-else
-    console.log("NOT ADULT")
-
 var i
 for(i=0;i<5;i++)
     console.log(i)
