0e187759c7797527f5f07b93f47350f308e7295a
itsam-13 <abhineet.am13@gmail.com>
2025-10-20 22:22:00 +0530

    Create a 2D array

---
+++ JavaScript/array2d.js
@@ -0,0 +1,12 @@
+let rows = 3;
+let cols = 3;
+let matrix = [];
+
+for (let i = 0; i < rows; i++) {
+    matrix[i] = []; // Create a row
+    for (let j = 0; j < cols; j++) {
+        matrix[i][j] = 0; // Initialize values (you can change this)
+    }
+}
+
+console.log(matrix);
